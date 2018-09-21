cff3c47bb94de030a7840a324d324dc16ce2622b
Anton Kopchak <anton.kopchak@vitech.com.ua>
2018-09-21 12:03:41 +0300

    change implementation of task

--- src/com/taskOne/Main.java
+++ src/com/taskOne/Main.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        String word;
+        String reversedWord = "";
+
         Scanner input = new Scanner(System.in);
         System.out.print("Enter word: ");
-        String word = input.nextLine();
-        StringBuffer reversedWord = new StringBuffer(word).reverse();
+
+        word = input.nextLine();
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+
         if (word.equals("exit")) {
             System.out.println("I left the program! Bye!!");
         } else if (word.contentEquals(reversedWord)) {
